cb98299960047c6a6010c836b3ce6dfeacce6d91
Nate Almanza <natealmanza3@gmail.com>
2026-04-10 16:04:34 -0700

    ADDED: initial routes

---
+++ src/routes/hello.js
@@ -0,0 +1,26 @@
+
+
+
+// MARK: HASIMAR ROUTE 
+// TODO: ADD /hello/HASIMAR route 
+
+
+
+
+
+// MARK: HARLEEN ROUTE
+// TODO: ADD /hello/HARLEEN route 
+
+
+
+
+
+// MARK: JOHNATHAN ROUTE 
+// TODO: ADD /hello/JOHNATHAN route
+
+
+
+
+
+// MARK: NATE ROUTE 
+// TODO: ADD /hello/NATE route
