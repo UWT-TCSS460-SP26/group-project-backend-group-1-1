688cfd58b7642eabedff7466e6ddd21a7521601a
Nate Almanza <natealmanza3@gmail.com>
2026-04-10 23:12:54 -0700

    Addded Health Route (Broken)

--- src/routes/hello/jonathan.ts
+++
@@ -1,7 +0,0 @@
-// Return a JSON greeting
-
-import { Request, Response } from 'express';
-
-export const jonathan = (request: Request, response: Response) => {
-  response.json({ message: 'Hello, Jonathan!' });
-};
