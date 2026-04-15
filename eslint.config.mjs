75609846288829546a8d96328479ac1e5cc222a4
Nate Almanza <natealmanza3@gmail.com>
2026-04-14 18:03:40 -0700

    fixed lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,10 @@
 import js from '@eslint/js';
-import prettierConfig from 'eslint-config-prettier';
 import tsPlugin from '@typescript-eslint/eslint-plugin';
 import tsParser from '@typescript-eslint/parser';
 import prettierConfig from 'eslint-config-prettier';
 
 export default [
   js.configs.recommended,
-  prettierConfig,
   {
     files: ['src/**/*.ts'],
     languageOptions: {
